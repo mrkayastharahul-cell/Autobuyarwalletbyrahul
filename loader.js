a2135eb49ca90d5eda21127f0e90290af9c185db
mrkayastharahul-cell <mr.kayastharahul@gmail.com>
2026-04-26 12:41:28 -0700

    Create loader.js

---
+++ loader.js
@@ -0,0 +1,5 @@
+javascript:(function(){
+  const s = document.createElement('script');
+  s.src = 'https://raw.githubusercontent.com/mrkayastharahul-cell/Autobuyarwalletbyrahul/main/main.js';
+  document.body.appendChild(s);
+})();
